205ce7961a677f1df4b43bda9d720681ebe0b000
tanakacha <kntasihc20.18@gmail.com>
2025-06-13 04:48:40 +0900

    feat: 筋トレ時間を反映 #12

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:http/http.dart' as http;
 import 'package:pksha/notification/models/notification_model.dart';
 import 'package:pksha/notification/services/notification_service.dart';
 import 'package:pksha/veiw/home/home_screen.dart';
@@ -16,4 +19,20 @@ void main() async {
   runApp(
     const ProviderScope(child: HomeScreen()),
   );
+}
+
+// バックエンドとの接続
+Future<String> fetchWorkoutTime() async {
+  final url = Uri.parse('http://localhost:8000/workout-time');
+  try {
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      return data['time'] ?? 'No time available';
+    } else {
+      throw Exception('Failed to fetch workout time: ${response.statusCode}');
+    }
+  } catch (e) {
+    throw Exception('Error: $e');
+  }
 }
